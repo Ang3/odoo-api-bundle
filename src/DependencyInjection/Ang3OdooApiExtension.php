99b9e8ba559d3cfd82efdb4211c86e339cb1660f
Joanis Rouanet <joanis.ang3@gmail.com>
2020-06-01 23:28:10 +0200

    Fix bundle extension and autowiring

--- src/DependencyInjection/Ang3OdooApiExtension.php
+++ src/DependencyInjection/Ang3OdooApiExtension.php
@@ -5,7 +5,6 @@ namespace Ang3\Bundle\OdooApiBundle\DependencyInjection;
 use Ang3\Bundle\OdooApiBundle\ClientRegistry;
 use Ang3\Component\Odoo\Client;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\DependencyInjection\Alias;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Exception\InvalidArgumentException;
@@ -27,18 +26,12 @@ class Ang3OdooApiExtension extends Extension
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
-
-        // Bundle parameters
         $container->setParameter('ang3_odoo_api.parameters', $config);
 
-        // Chargement des connections
-        $this->loadClientRegistry($container, $config['connections'], $config['default_connection']);
-
-        // Définition d'un chargeur de fichier YAML
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-
-        // Chargement des services
         $loader->load('services.yaml');
+
+        $this->loadClientRegistry($container, $config['connections'], $config['default_connection']);
     }
 
     /**
@@ -46,18 +39,14 @@ class Ang3OdooApiExtension extends Extension
      */
     public function loadClientRegistry(ContainerBuilder $container, array $connections, string $defaultConnection): void
     {
-        // Si la connexion par défat n'existe pas
         if (!array_key_exists($defaultConnection, $connections)) {
             throw new InvalidArgumentException(sprintf('The default Odoo connection "%s" is not configured', $defaultConnection));
         }
 
-        // Création de la définition du registre
-        $registry = new Definition(ClientRegistry::class);
+        $registry = $container->getDefinition(ClientRegistry::class);
         $odooLogger = $container->hasDefinition(self::MONOLOG_DEFINITION) ? new Reference(self::MONOLOG_DEFINITION) : null;
 
-        // Pour chaque conenctions
         foreach ($connections as $name => $params) {
-            // Création de la définition
             $client = new Definition(Client::class, [[
                 $params['url'],
                 $params['database'],
@@ -65,34 +54,18 @@ class Ang3OdooApiExtension extends Extension
                 $params['password'],
             ], $odooLogger]);
 
-            // Définition du nom du client
             $clientName = sprintf('ang3_odoo_api.client.%s', $name);
-
-            // Enregistrement du client dans le container
             $container->setDefinition($clientName, $client);
             $container->registerAliasForArgument($clientName, Client::class, "$name.client");
 
-            // S'il s'agit du client par défaut
             if ($name === $defaultConnection) {
-                // Enregistrement du client par défaut
                 $container->setDefinition(Client::class, $client);
-
-                // Enregistrement du client par défaut
                 $container->setAlias('ang3_odoo_api.client', $clientName);
                 $container->registerAliasForArgument($clientName, Client::class, 'client');
             }
 
-            // Retour de la référence du service client
             $clientReference = new Reference($clientName);
-
-            // Enregistrement de la connection dans le registre
             $registry->addMethodCall('set', [$name, $clientReference]);
         }
-
-        // Enregistrement du registre dans le container
-        $container->setDefinition(ClientRegistry::class, $registry);
-
-        // Enregistrement de l'alias
-        $container->setAlias('ang3_odoo_api.client_registry', new Alias(ClientRegistry::class, true));
     }
 }
